554a574fae5677dfe1b45c3423bee4d0b3f731cd
IvanIvanov0728 <ivan.ivanov.highschool@buditel.bg>
2025-01-14 10:26:37 +0200

    searchaIsWorkingYesssssssssss

--- Filmtivity-Skeleton/scripts/search.js
+++ Filmtivity-Skeleton/scripts/search.js
@@ -1,24 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("search-btn").addEventListener('click', function() {
-        var title = document.getElementById("search").value;
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('search-btn').addEventListener('click', function() {
+        var title = document.getElementById('search').value;
         if(title.trim()){
             fetchMovie(title);
-            document.getElementById("details-page").style.display = 'block';
+            document.getElementById('details-page').style.display = 'block';
         }else{
-            document.getElementById("details-page").style.display = 'none';
+            document.getElementById('details-page').style.display = 'none';
         }
     });
 });
 
 function fetchMovie(title) {
- fetch('https://api.themoviedb.org/3/search/movie?api_key=4b42cd75f9ee878cac9bf18dc90f1ea5&query=${title}')
+ fetch(`https://api.themoviedb.org/3/search/movie?api_key=4b42cd75f9ee878cac9bf18dc90f1ea5&query=${title}`)
     .then(response => response.json())
     .then(data => {
-        document.getElementById("title").textContent = data.results[0].title;
-        document.getElementById("release-date").textContent = data.results[0].release_date;
-        document.getElementById("overview").textContent = data.results[0].overview;
-        document.getElementById("vote_average").textContent = data.results[0].vote_average;
-        document.getElementById("original_language").textContent = data.results[0].original_language;[0].original_language;
-        document.getElementById("poster").src = `https://image.tmdb.org/t/p/w500${data.results[0].poster_path}`;
+        const movie = data.results[0]
+        document.getElementById('title').textContent = movie.title;
+        document.getElementById('release-date').textContent = movie.release_date;
+        document.getElementById('overview').textContent = movie.overview;
+        document.getElementById('vote_average').textContent = movie.vote_average;
+        document.getElementById('original_language').textContent = movie.original_language;
+        document.getElementById('poster').src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
     }).catch(error => console.error(error));
 }
